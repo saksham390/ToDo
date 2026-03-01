3d09551b53ecc262b2d91d424229d225347261fe
Saksham <sakshamkhurana23@gmail.com>
2026-03-01 23:03:27 +0530

    Add to-do list functionality in script.js
    Implement a basic to-do list with task addition, rendering, and deletion functionality.

---
+++ script.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded',() =>{
+    const todoInput = document.getElementById("todo-input");
+const addTaskButton = document.getElementById("add-task-btn");
+const todoList = document.getElementById("todo-list");
+
+
+
+let tasks=JSON.parse(localStorage.getItem('task')) || [];
+tasks.forEach(task=>renderTask(task));
+
+addTaskButton.addEventListener('click',() => {
+const taskText=todoInput.ValueMax.trim()
+if(task==="") return;
+const newTask={
+    id:Date.now(),
+    text:taskText,
+    completed:false,
+}
+tasks.push(newTask)
+saveTasks();
+renderTasks(newtask);
+todoInput.value="";
+console.log(tasks);
+});
+
+function renderTask(task){
+
+   const li=document.createElement(li);
+   li.setAttribute('data-id',task.id);
+   
+   li.innerHTML=` <span>${task.text}</span>
+<button> delete</button>`;
+li.addEventListener('click',(e)=>{
+    if(e.target.tagName == 'BUTTON') return;
+    task.completed=!task.completed
+    li.classList.toggle('completed')
+    saveTasks();
+
+
+});
+
+li.querySelector('button').addEventListener('click',(e)=>{
+    e.stopPropagation()
+    tasks=tasks.filter(t=>t.id!=task.id)
+    li.remove();
+    saveTasks();
+})
+todoList.appendChild(li);
+  
+}
+
+function saveTasks(){
+    localStorage.setItem('tasks',JSON.stringify(tasks))
+}
+})
